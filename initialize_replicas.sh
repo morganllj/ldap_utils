878c0aaaa98a0aad5871342e8b5b0e16c72234d3
Morgan Jones <morgan@morganjones.org>
2012-02-14 22:03:40 +0000

    initial version

---
+++ initialize_replicas.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+
+host=ldaps://ldapm01.domain.org  # -H style ldapurl
+pass=pass
+
+ldapsearch -x -H $host -D cn=directory\ manager -w $pass -s one \
+    -LLLb  cn=replica,cn=\"dc=domain,dc=org\",cn=mapping\ tree,cn=config \
+    objectclass=nsds5replicationagreement dn |
+perl -0000 -n -e '
+    s/\n\s//;
+    s/\n//;
+    print; 
+    print "changetype: modify\n";
+    print "replace: nsds5beginreplicarefresh\n";
+    print "nsds5beginreplicarefresh: start\n\n"
+'|
+ldapmodify -H $host -D cn=directory\ manager -w $pass
