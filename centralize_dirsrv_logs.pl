93127f85660b0a3a64722a3eac9c72c8f9cfeff8
Morgan Jones <morgan@morganjones.org>
2013-02-01 15:21:58 -0500

    updated to include ldap0 and ldap3 whose paths are different than centos directory.  It's a bit of a hack.

--- centralize_dirsrv_logs.pl
+++ centralize_dirsrv_logs.pl
@@ -4,25 +4,15 @@
 #
 use strict;
 
-my @dir_hosts = qw/ldapm01-mgmt ldapm02-mgmt ldap01-mgmt ldap02-mgmt devldap01-mgmt devldap02-mgmt/;
+my @dir_hosts = qw/ldapm01-mgmt ldapm02-mgmt ldap01-mgmt ldap02-mgmt devldap01-mgmt devldap02-mgmt ldap0 ldap3/;
 my $log_host="rsyslog1.oit.domain.org";
-
-# my $short_host=`hostname`;
-# chomp $short_host;
-# $short_host=~s/([^\.]+)\..*/$1/;
-# my $instance="slapd-".$short_host;
 
 my $remote_location="/var/log/dirsrv";
 my $local_location="/var/log/dirsrv";
 my @logs=qw/access errors audit/;
 
-
-
-# ssh root@devldap01.domain.org "rsync -n -avHe ssh /var/log/dirsrv/slapd-devldap01/ rsyslog1.oit.domain.org:/var/log/dirsrv/slapd-devldap01"
-
 for my $dir_host (@dir_hosts) {
     my $short_host=$dir_host;
-#    chomp $short_host;
     $short_host=~s/([^\.]+)\..*/$1/;
     my $instance="slapd-".$short_host;
     $instance =~ s/-mgmt$//;
@@ -32,7 +22,13 @@ for my $dir_host (@dir_hosts) {
     my $cmd="ssh -l root ${dir_host} \"rsync --progress -avHe ssh ";
 
     for my $l (@logs) {
-	$cmd .= "$local_location/". $instance . "/${l}* "
+	if ($dir_host eq "ldap0") {
+	    $cmd .= "/var/Sun/mps/slapd-ldap0/logs/${l}* "
+	} elsif ($dir_host eq "ldap3") {
+	    $cmd .= "/var/Sun/mps/slapd-ldap3/logs/${l}* "
+	} else {
+	    $cmd .= "$local_location/". $instance . "/${l}* "
+	}
     }
 
     $cmd .= " $log_host:" . $remote_location . "/" . $instance . "\"";
