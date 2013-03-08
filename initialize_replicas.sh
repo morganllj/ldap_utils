0c9d9906b7a304eefa430c0c62ffeb1c2a5fe379
Morgan Jones <morgan@morganjones.org>
2013-03-08 15:50:08 -0500

    added command line arguments to chose master and consumers

--- initialize_replicas.sh
+++ initialize_replicas.sh
@@ -1,12 +1,47 @@
 #!/bin/sh
 #
 
-host=ldaps://ldapm01.domain.org  # -H style ldapurl
-pass=pass
+function print_usage {
+    echo "usage: $ARGV[0] <hostname> [<consumer1> <consumer2> ...]"
+    echo "    <host> needs to be an ldap master and an ldapurl: ldaps://host.domain"
+    echo "    consumers are just hostnames, not ldapurls."
+    echo
+    exit
+}
 
-ldapsearch -x -H $host -D cn=directory\ manager -w $pass -s one \
+if [ -z $1 ]; then
+    print_usage
+fi
+
+master=$1
+
+for host in $@; do
+    if [ $host != $master ]; then
+        consumers=$consumers" "$host
+    fi
+done
+
+echo initializing $consumers on $master...
+echo
+
+host=$master  # -H style ldapurl
+pass=pass
+bind="cn=directory\\ manager"
+
+filter='(|(objectclass=nsds5replicationagreement)(objectclass=nsDSWindowsReplicationAgreement))'
+if [ ! -z "$consumers" ]; then
+    filter=${filter}"(|"
+    for consumer in $consumers; do
+        filter=${filter}"(nsDS5ReplicaHost=$consumer)"
+    done
+    filter="(&${filter}))"
+fi
+
+echo $filter
+
+ldapsearch -x -H $host -D "$bind" -w $pass -s one \
     -LLLb  cn=replica,cn=\"dc=domain,dc=org\",cn=mapping\ tree,cn=config \
-    '(|(objectclass=nsds5replicationagreement)(objectclass=nsDSWindowsReplicationAgreement))' dn |
+     "$filter" dn |
 perl -0000 -n -e '
     s/\n\s//;
     s/\n//;
@@ -15,4 +50,6 @@ perl -0000 -n -e '
     print "replace: nsds5beginreplicarefresh\n";
     print "nsds5beginreplicarefresh: start\n\n"
 '|
-ldapmodify -H $host -D cn=directory\ manager -w $pass
+ldapmodify -H $host -D "$bind" -w $pass
+
+
