3af223f26480224156de465bd719e178c6525afe
Morgan Jones <morgan@morganjones.org>
2013-09-10 15:59:34 -0400

    first rev, simple functionality

---
+++ ldif2csv.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl -w
+#
+$/="";
+
+while (<>) {
+    s/\n //;
+
+    my @e = sort split /\n/;
+    my $c=0;
+
+    for (@e) {
+	chomp;
+
+	@l=split /:\s+/;
+
+	if (/^dn:/) {
+	    $c++;
+	    next;
+	}
+
+	print "\""
+	  if (/\,/);
+
+	if (/::/) {
+	    print `echo $l[1] | openssl base64 -d`;
+	} else {
+	    print $l[1]
+	      if (defined $l[1]);
+	}
+
+	print "\""
+	  if (/\,/);
+
+	print "," if (++$c <= $#e);
+    } 
+
+    print "\n"
+}
