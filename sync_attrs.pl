069c6f959ed0bdaff0cf534bfea7d36af546b811
morgan jones <morgan@morganjones.org>
2016-04-30 14:09:43 -0400

    moving toward generalizing (though a lot is still hardcoded) prints values as cvs when there is a mismatch this works as I remember but is rough

--- sync_attrs.pl
+++ sync_attrs.pl
@@ -1,11 +1,10 @@
 #!/usr/bin/perl -w
 #
-
-# ldapsearch -w pass -h host -x -LLLb base -D uid=morgan,ou=people,dc=domain,dc=org '(&(|(objectclass=orgperson)(objectclass=orgnonperson))(orgactiveinactive=i))' uid orghrexpirationdate| ./convert_expiration_dates.pl
+# set values in one ldap based on values in another ldap.  
+#
+# Values hardcoded, needs work.
 
 use strict;
-#use Date::Pcalc qw(Delta_Days);
-#use Time::Local;
 use Net::LDAP;
 use Getopt::Std;
 use strict;
@@ -21,8 +20,12 @@ if (exists $opts{n}) {
     print "\n";
 };
 
+#my @attrs = qw/zimbraaccountstatus zimbramailstatus zimbrahideingal:empty=false/;
+my @attrs = qw/zimbraaccountstatus zimbramailstatus zimbrahideingal/;
+my @desired_values = qw/active enabled FALSE/;
 
-my $ldap = Net::LDAP->new("ldaps://ldap.domain.net") || die "$@";
+
+my $ldap = Net::LDAP->new("ldaps://ldap.domain.org") || die "$@";
 my $bind_rslt = $ldap->bind("cn=directory manager", password=>"pass");
 $bind_rslt->code && die "problem binding: ", $bind_rslt->error;
 
@@ -31,17 +34,48 @@ my $new_bind_rslt = $new_ldap->bind("cn=config", password=>"pass");
 $new_bind_rslt->code && die "problem binding to new ldap: ", $bind_rslt->error;
 
 my $rslt = $ldap->search(base=>"dc=domain,dc=org", 
-			 filter=>"(&(objectclass=orgemployee)(orgaccountactive=FALSE))",
+			 filter=>"(&(objectclass=orgemployee)(orgaccountactive=TRUE))",
 			 attrs=>["uid","orgaccountactive"]);
 $rslt->code && die "problem searching: ", $rslt->error;
 
 for my $entry ($rslt->entries) {
     my $uid = $entry->get_value("uid");
 
+
+    # not working but +/- what it should look like
+    # my $k = 0;
+    # for (@attrs) {
+    # 	if ($attrs[$j] =~ /:/) {
+    # 	    my ($lhs, $rhs) = split (/:/, $desired_values[$j]);
+    # 	    print "colon $lhs $rhs\n";
+    # 	    if ($rhs =~ /=/) {
+    # 		my ($e_lhs, $e_rhs) = split (/=/, $rhs);
+    # 		print "equals: $e_lhs $e_rhs\n";
+    # 		if ($e_lhs =~ /empty/) {
+    # 		    if ($e_rhs =~ /true/) {
+    # 			# drop through to comparison below
+    # 			$current_values[$i] = "false";				    
+    # 		    } elsif ($e_rhs =~ /false/) {
+    # 			$current_values[$i] = "false";
+    # 		    } else {
+    # 			die "lhs of '=' in $desired_values[$i] must be true or false";
+    # 		    }
+    # 		} else {
+    # 		    die "lhs of '=' in $desired_values[$i] can only be 'empty'";
+    # 		}
+    # 	    } else {
+    # 		die "$desired_values[$i] includes a ':' without an '='";
+    # 	    }
+    # 	}
+    # }
+
+
+
     my $dn = $entry->dn;
 	my $new_rslt = $new_ldap->search(base=>"", 
 				 filter=>"(&(uid=$uid)(objectclass=zimbraaccount))",
-				 attrs=>["objectclass", "uid", "zimbraaccountstatus", "zimbramailstatus", "zimbrahideingal"]);
+#				 attrs=>["objectclass", "uid", "zimbraaccountstatus", "zimbramailstatus", "zimbrahideingal"]);
+				 attrs=>["objectclass", "uid", @attrs]);
 	$new_rslt->code && die "problem searching: ", $new_rslt->error;
 
 	my @new_entries = $new_rslt->entries;
@@ -53,24 +87,54 @@ for my $entry ($rslt->entries) {
 	    my $new_entry = $new_entries[0];
 	    my $new_dn = $new_entry->dn;
 
-	    my $accountstatus = $new_entry->get_value("zimbraaccountstatus");
-	    my $mailstatus = $new_entry->get_value("zimbramailstatus");
-	    my $hideingal = $new_entry->get_value("zimbrahideingal");
+	    # my $accountstatus = $new_entry->get_value("zimbraaccountstatus");
+	    # my $mailstatus = $new_entry->get_value("zimbramailstatus");
+	    # my $hideingal = $new_entry->get_value("zimbrahideingal");
+
+	    my @current_values;
+	    my $i=0;
+	    for my $attr (@attrs) {
+		$current_values[$i] = $new_entry->get_value($attr);
+		$current_values[$i] = "" 
+		  if (!defined $current_values[$i]);
+		$i++
+	    }
 
 
-	    unless ($accountstatus eq "closed" && $mailstatus eq "disabled" && $hideingal eq "TRUE") {
-		print "$uid,$accountstatus,$mailstatus,";
-		print $hideingal if (defined $hideingal);
+#	    unless ($accountstatus eq "closed" && $mailstatus eq "disabled" && $hideingal eq "TRUE") {
+	    my $mismatch = 0;
+	    my $j=0;
+	    for (@desired_values) {
+		if ($attrs[$j] =~ /zimbrahideingal/) {
+		    if ($current_values[$j] =~ /^\s*$/) {
+			$current_values[$j] = "FALSE";
+		    }
+		}
+		
+		if (lc $desired_values[$j] ne lc $current_values[$j]) {
+		    $mismatch = 1
+		}
+
+		$j++
+	    }
+	      
+	    if ($mismatch) {
+#		print "$uid,$accountstatus,$mailstatus,";
+		print "$uid,";
+		for my $value (@current_values) {
+		    print $value . ",";
+		}
+#		print $hideingal if (defined $hideingal);
 	    
 		unless (exists $opts{n}) {
-		    my $new_mod_result = $new_ldap->modify($new_dn,
-							   replace => {"zimbraaccountstatus" => "closed",
-								       "zimbramailstatus" => "disabled",
-								       "zimbrahideingal" => "TRUE"
-								      },
-		    				      );
-		    $new_mod_result->code && die "problem modifying new ldap: ", $new_mod_result->error;
-		    print " successfully modified";
+		    # my $new_mod_result = $new_ldap->modify($new_dn,
+		    # 					   replace => {"zimbraaccountstatus" => "closed",
+		    # 						       "zimbramailstatus" => "disabled",
+		    # 						       "zimbrahideingal" => "TRUE"
+		    # 						      },
+		    # 				      );
+		    # $new_mod_result->code && die "problem modifying new ldap: ", $new_mod_result->error;
+		    # print " successfully modified";
 		}
 		print "\n";
 	    }
