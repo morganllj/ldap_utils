1ddc07421a76e4dd5ed288a5ae722467489b2343
Morgan Jones <morgan@morganjones.org>
2017-04-19 01:15:50 -0400

    simple script to hold open an ldap connection to test timeouts

---
+++ hold_open_ldap_con.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+#
+
+use Net::LDAP;
+use Data::Dumper;
+use strict;
+
+my $ldap = Net::LDAP->new("ldaps://host") || die "problem connecting";
+my $br = $ldap->bind(dn=>"cn=directory manager", password=>"pass");
+$br->code && die $br->error;
+my $sr = $ldap->search(base=>"dc=domain,dc=org", filter=>"uid=morgan", attrs=>['uid'] );
+
+$sr->code && die $sr->error;
+my @e = $sr->entries;
+
+print Dumper @e;
+
+sleep 600;
+print "done.\n";
